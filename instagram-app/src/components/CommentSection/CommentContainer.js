adb716bd7f7738bc0078db2d316a8dc2464b3831
Anna W <anski.anna@gmail.com>
2019-06-04 13:41:04 +0100

    refactored commentSection into class component

--- instagram-app/src/components/CommentSection/CommentContainer.js
+++ instagram-app/src/components/CommentSection/CommentContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Comments from '/Users/Anna/Lambda School/Sprint 7 - Intermediate React/Day 1 - React Tooling/React-Insta-Clone/instagram-app/src/components/CommentSection/Comments.js';
 import pt from 'prop-types';
 // import CommentInput from '/Users/Anna/Lambda School/Sprint 7 - Intermediate React/Day 1 - React Tooling/React-Insta-Clone/instagram-app/src/components/CommentSection/CommentInput.js';
@@ -25,30 +25,40 @@ import pt from 'prop-types';
 //     }
 // }
 
-const CommentSection = ({ likes, postTime, comments}) => {
-    return (
-        <div>
-            <div>{likes} likes</div>
-                {comments.map((comment, index) => (
-                    <Comments 
-                    key={index}
-                    username={comment.username}
-                    text={comment.text}
-                    />
-                    )
-                )}
+class CommentSection extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            comments: props.comments,
+        }
+    }
 
+    render() {
+        return (
             <div>
-                <input
-                    type="text"
-                    placeholder="Add a comment"
-                />
-            </div>
-
-            </div>
-      
-    )
+                <div>{this.props.likes} likes</div>
+                    {this.state.comments.map((comment, index) => (
+                        <Comments 
+                        key={index}
+                        username={comment.username}
+                        text={comment.text}
+                        />
+                        )
+                    )}
+    
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Add a comment"
+                    />
+                </div>
+    
+                </div>
+          
+        )
+    }
 }
+
 
 CommentSection.propTypes = {
     comments: pt.arrayOf(
