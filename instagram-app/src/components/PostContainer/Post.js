1b720bb744668906ad9f9c7704ca69e330e03515
Anna W <anski.anna@gmail.com>
2019-06-06 13:53:29 +0100

    changed styling in the styled component for posts

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -5,29 +5,48 @@ import styled from 'styled-components';
 
 const PostStyle = styled.div`
     border: 1px solid grey;
-    padding: none;
+    margin: 20px;
+    width: 90%;
+    
+    img {
+       width: 50%;
+
+    }
+`;
+const UserinfoStyle = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    border: 1px solid red;
+
+    img {
+        margin: 20px;
+        border-radius: 100%;
+        width:30px;
+    }
+
 `;
 
-const ThumbnaailStyle = styled.img`
-    border-radius: 30px;
-    size: 40px;
-    width: 50px;
-`;
+// const ThumbnaailStyle = styled.img`
+//     border-radius: 100%;
+//     size: 40px;
+//     width: 50px;
+// `;
 
 
 
 const Post = ({ thumbnailUrl, username, postImageUrl }) => {
     return (
-        <PostStyle className="post">
-            <div className="post-info">
+        <PostStyle>
+            <UserinfoStyle>
                 <div>
-                    <ThumbnaailStyle
+                    <img
                         src={thumbnailUrl}
                         alt="User Img" 
                     />
                 </div>
                 <h4>{username}</h4>
-            </div>
+            </UserinfoStyle>
 
             <div className="post-img">
                 <img 
